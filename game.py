3a812f9784eddd172694223cda24a95f6dc4e253
lexiedooley <lexiedools@gmail.com>
2023-07-06 11:39:26 -0400

    adding upgrades

---
+++ game.py
@@ -0,0 +1,9 @@
+game = {'tool': 0, 'money':0}
+
+tools = [
+    {'name': 'teeth', 'profit': 1, 'cost': 0},
+    {'name': 'rusty_scissors', 'profit': 5, 'cost': 5},
+    {'name': 'push_lawnmower', 'profit': 50, 'cost': 25},
+    {'name': 'fancy_lawnmower', 'profit': 100, 'cost': 250},
+    {'name': 'starving_students', 'profit': 250, 'cost': 500}
+]
